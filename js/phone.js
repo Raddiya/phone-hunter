62f9c8e5589a7ee5e92b86eab92e4cd9f43cf41d
Raddiya <raddiyasultana.612@gmil.com>
2022-03-02 01:32:11 +0600

    phone-world done

--- js/phone.js
+++ js/phone.js
@@ -1,35 +1,42 @@
 const searchPhone = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    // Clear data
-    searchField.value = '';
+    const errorShow = document.getElementById('error-text')
+    errorShow.textContent = ''
 
-    const url = ` https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    const displaySearch = document.getElementById('search-result');
+    displaySearch.textContent = '';
 
-    fetch(url)
-        .then(res => res.json())
-        .then(data => searchResult(data.data));
+    if (searchText == '') {
+        errorShow.innerHTML = "No phone found!!";
+    }
+    else {
+        // Clear data
+        searchField.value = '';
+        const url = ` https://openapi.programming-hero.com/api/phones?search=${searchText}`
+
+        fetch(url)
+            .then(res => res.json())
+            .then(data => searchResult(data.data));
+    }
 }
 
 const searchResult = data => {
-    // console.log(data);
     const displaySearch = document.getElementById('search-result');
     displaySearch.textContent = '';
-    // if () {
 
-    // }
-    data.forEach(phone => {
+    data.slice(0, 20).forEach(phone => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
         <div class="col">
-                <div class="card bg-success p-2 text-white bg-opacity-75">
-                    <img src="${phone.image}" class="card-img-top img-fluid " alt="...">
-                    <div class="card-body back">
+                <div class="card bg-success p-2 text-white bg-opacity-50">
+                    <img src="${phone.image}" class="card-img-top w-50 mx-auto" alt="...">
+                    <div class="card-body text-center">
                     <h4>${phone.brand}</h4>
-                        <h5 class="card-title">${phone.phone_name}</h5>
+                        <h5 class="card-title text-center">${phone.phone_name}</h5>
                      </div>
-                    <button onclick="loadPhoneDetail('${phone.slug}')" class="btn btn-primary text-center me-md-2 w-25" type="button">Detail</button>
+                    <button onclick="loadPhoneDetail('${phone.slug}')" class="btn btn-primary  mx-auto w-25" type="button">Detail</button>
                 </div>
                 
          </div>      
@@ -39,7 +46,6 @@ const searchResult = data => {
 }
 const loadPhoneDetail = phoneId => {
     const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
-
     fetch(url)
         .then(res => res.json())
         .then(data => displayPhoneDetail(data.data));
@@ -56,9 +62,12 @@ const displayPhoneDetail = phone => {
     <div class="card-body">
    
         <h5 class="card-title">${phone.name}</h5>
-        <p class="card-text">Release date:${phone.releaseDate ? phone.releaseDate : ''}</p>
-        <p class="card-text">Others:${phone.others.GPS} ${phone.others.USB}</p>
-        <p class="card-text">Mainfeatures:${phone.mainFeatures.sensors}</p>
+        <h4>  Release date:</h4>
+        <p class="card-text">${phone.releaseDate ? phone.releaseDate : 'No release date found'}</p>
+        <h4>  Others:</h4>
+        <p class="card-text">${phone.others.GPS} ${phone.others.USB} ${phone.others.Bluetooth} ${phone.others.WLAN}</p>
+        <h4> Sensor info:</h4>
+        <p class="card-text">${phone.mainFeatures.sensors}</p>
     </div>
     `;
     phoneDetail.appendChild(div);
